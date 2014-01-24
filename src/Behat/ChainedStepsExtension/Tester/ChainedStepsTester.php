234707a501d567b1a4229fd1b1edf76cd7f1364f
Christophe Coevoet <stof@notk.org>
2014-01-24 23:12:17 +0100

    Refactored the ChainedStepsTester to avoid dispatching events substeps

--- src/Behat/ChainedStepsExtension/Tester/ChainedStepsTester.php
+++ src/Behat/ChainedStepsExtension/Tester/ChainedStepsTester.php
@@ -12,6 +12,8 @@ namespace Behat\ChainedStepsExtension\Tester;
 
 use Behat\Behat\Tester\HookableStepTester;
 use Behat\Behat\Tester\Result\HookedStepTestResult;
+use Behat\Behat\Tester\StepTester;
+use Behat\Behat\Tester\Result\StepTestResult;
 use Behat\Behat\Tester\Result\TestResult;
 use Behat\ChainedStepsExtension\Step\SubStep;
 use Behat\Gherkin\Node\FeatureNode;
@@ -25,15 +27,28 @@ class ChainedStepsTester extends HookableStepTester
     {
         $result = parent::testStep($environment, $feature, $step, $skip);
 
+        return $this->processTestResult($environment, $feature, $result, $skip);
+    }
+
+    /**
+     * @param Environment    $environment
+     * @param FeatureNode    $feature
+     * @param StepTestResult $result
+     * @param boolean        $skip
+     *
+     * @return StepTestResult
+     */
+    private function processTestResult(Environment $environment, FeatureNode $feature, StepTestResult $result, $skip)
+    {
         $callResult = $result->getCallResult();
 
-        if (null === $callResult || !$this->supportsResult($callResult)) {
+        if (null === $callResult || !$this->supportsResult($callResult) || TestResult::PASSED < $result->getResultCode()) {
             return $result;
         }
 
-        $skip = $skip || TestResult::PASSED < $result->getResultCode();
-
-        return $this->runChainedSteps($environment, $feature, $result, $skip);
+        $result = $this->runChainedSteps($environment, $feature, $result, $skip);
+        var_dump($result->getException(), $result->getResultCode());
+        return $result;
     }
 
     private function supportsResult(CallResult $result)
@@ -57,7 +72,7 @@ class ChainedStepsTester extends HookableStepTester
         return true;
     }
 
-    private function runChainedSteps(Environment $environment, FeatureNode $feature, HookedStepTestResult $result, $skip)
+    private function runChainedSteps(Environment $environment, FeatureNode $feature, StepTestResult $result, $skip)
     {
         $callResult = $result->getCallResult();
         $steps = $callResult->getReturn();
@@ -68,11 +83,73 @@ class ChainedStepsTester extends HookableStepTester
 
         /** @var SubStep[] $steps */
 
+        $results = array($result);
+
         foreach ($steps as $step) {
-            $result = $this->testStep($environment, $feature, $step, $skip);
-            $skip = $skip || TestResult::PASSED < $result->getResultCode();
+            $results[] = $stepResult = $this->testSubStep($environment, $feature, $step, $skip);
+            $skip = $skip || TestResult::PASSED < $stepResult->getResultCode();
         }
 
-        return $result;
+        return $this->mergeStepResults($results);
+    }
+
+    /**
+     * @param Environment $environment
+     * @param FeatureNode $feature
+     * @param StepNode    $step
+     * @param boolean     $skip
+     *
+     * @return StepTestResult
+     */
+    private function testSubStep(Environment $environment, FeatureNode $feature, StepNode $step, $skip)
+    {
+        $result = StepTester::testStep($environment, $feature, $step, $skip);
+
+        return $this->processTestResult($environment, $feature, $result, $skip);
+    }
+
+    /**
+     * @param StepTestResult[] $results
+     *
+     * @return StepTestResult
+     */
+    private function mergeStepResults(array $results)
+    {
+        $originalResult = $results[0];
+
+        $searchException = null;
+        $exception = null;
+        $stdOut = null;
+        $call = null;
+        $return = null;
+
+        foreach ($results as $result) {
+            if (null === $searchException) {
+                $searchException = $result->getSearchException();
+            }
+
+            if (null !== $callResult = $result->getCallResult()) {
+                if (null === $exception) {
+                    $exception = $callResult->getException();
+                }
+
+                if ($callResult->hasStdOut()) {
+                    $stdOut .= $callResult->getStdOut();
+                }
+            }
+        }
+
+        if (null !== $originalResult->getCallResult()) {
+            $call = $originalResult->getCallResult()->getCall();
+            $return = $originalResult->getCallResult()->getReturn();
+        }
+
+        $finalCallResult = new CallResult($call, $return, $exception, $stdOut);
+
+        if ($originalResult instanceof HookedStepTestResult) {
+            return new HookedStepTestResult($originalResult->getSearchResult(), $searchException, $finalCallResult, $originalResult->getHookCallResults());
+        }
+
+        return new StepTestResult($originalResult->getSearchResult(), $searchException, $finalCallResult);
     }
 }
